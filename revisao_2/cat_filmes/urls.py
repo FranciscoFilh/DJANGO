f6483bee3cb2bcc5392d44543658d119f8a10782
francisco oliveira <fs9457489@gmail.com>
2024-01-30 19:56:30 -0300

    revisao

--- revisao_2/cat_filmes/urls.py
+++ revisao_2/cat_filmes/urls.py
@@ -1,16 +1,16 @@
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-from .views import index, cadastro, delete
+from .views import index, cadastro, deletar
 from rest_framework import routers
 from .api.viewsets import FilmeViewSet
 
 router = routers.DefaultRouter()
-router.register(r'api_filmes', FilmeViewSet)
+router.register('api_filmes', FilmeViewSet)
 
 urlpatterns = [
     path('', index, name='index'),
     path('cadastro/', cadastro, name='cadastro'),
-    path('delete/<int:id>', delete, name='delete'),
+    path('deletar/<int:id>', deletar, name='deletar'),
     path('api_filmes/', include(router.urls)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- revisao_2/cat_filmes/views.py
+++ revisao_2/cat_filmes/views.py
@@ -5,25 +5,28 @@ from .forms import FilmeForm
 
 def index(request):
     filmes = Filme.objects.all()
-    return render(request, 'cat_filmes/index.html', {'filmes': filmes})
+    context = {
+        'filmes': filmes, 
+    }
+    return render(request, 'cat_filmes/index.html', context)
 
 def cadastro(request):
     if request.method == 'POST':
         form = FilmeForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            messages.success(request, 'Carro adicionado com sucesso')
+            messages.success(request, 'Filme cadastrado com Sucesso!')
             form = FilmeForm()
         else:
-            messages.error(request, 'Erro ao adicionar carro')
+            messages.error(request, 'Erro ao salva o Filme!')
     else:
         form = FilmeForm()
+    
+    return render(request, 'cat_filmes/cadastro.html', { 'form': form})
 
-    return render(request, 'cat_filmes/cadastro.html', { 'form': form })
-
-def delete(request, id):
+def deletar(request, id):
     deleteme = Filme.objects.get(id=id)
     if request.method == 'POST':
         deleteme.delete()
         return redirect('/')
-    return render(request, 'cat_filmes/delete.html')
+    return render(request, 'cat_filmes/deletar.html')
